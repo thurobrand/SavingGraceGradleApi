4c92fd7a1437e5571a537a73279298318adbfe78
Jonathan Keen <thurobrand@gmail.com>
2022-10-02 21:24:14 -0400

    InitialCommit

---
+++ src/main/java/com/savinggrace/apis/SavingGraceApisApplication.java
@@ -0,0 +1,13 @@
+package com.savinggrace.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingGraceApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingGraceApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savinggrace/apis/SavingGraceApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savinggrace.apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingGraceApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
